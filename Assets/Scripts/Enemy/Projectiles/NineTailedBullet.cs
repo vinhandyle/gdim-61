63eef723170a7ffc385c6fab818ee897d5ae0ebb
leannen2 <leannen2@uci.edu>
2022-05-10 15:03:08 -0700

    Update NineTailedBullet.cs

--- Assets/Scripts/Enemy/Projectiles/NineTailedBullet.cs
+++ Assets/Scripts/Enemy/Projectiles/NineTailedBullet.cs
@@ -18,14 +18,4 @@ public class NineTailedBullet : Projectile
         //StartCoroutine(TickDamage(player, 5, 3, 1));
         Destroy(gameObject);
     }
-
-    protected IEnumerator TickDamage(GameObject player, int damagePerTick, int numberOfTicks, float timeBetweenTicks)
-    {
-        yield return new WaitForSeconds(timeBetweenTicks);
-        if (numberOfTicks > 0)
-        {
-            player.GetComponent<Health>().TakeDamage(damagePerTick);
-            StartCoroutine(TickDamage(player, damagePerTick, numberOfTicks - 1, timeBetweenTicks));
-        }
-    } 
 }

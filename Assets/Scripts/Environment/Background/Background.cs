0c5740891fab6a5367de0b820241404ccc838010
Andy Le <52895718+vinhandyle@users.noreply.github.com>
2022-05-19 22:37:25 -0700

    Added red ogre animations, background parallax, and flame sprites

--- Assets/Scripts/Environment/Background/Background.cs
+++ Assets/Scripts/Environment/Background/Background.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Defines parallax behavior for backgrounds.
+/// </summary>
 public class Background : MonoBehaviour
 {
     [SerializeField] private Transform anchor;
+    [SerializeField] private float scale;
+    private float pivotPos;
+
+    private void Awake()
+    {
+        pivotPos = transform.position.x;
+    }
 
     void FixedUpdate()
     {
-        transform.position = new Vector3(anchor.position.x, transform.position.y, transform.position.z);
+        // Scrolling is determined by scaling distance from the pivot
+        // and adjusting background relative to the anchor
+        float delta = (anchor.position.x - pivotPos) * scale;
+        transform.position = new Vector3(anchor.position.x - delta, transform.position.y, transform.position.z);
     }
 }
